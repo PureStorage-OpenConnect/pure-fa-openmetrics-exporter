757def46bdd5385f1b5c89f51fbce3d13488f29c
Simon Dodsley <simondodsley@gmail.com>
2023-11-10 13:38:36 -0500

    Add purefa_drive metric

---
+++ internal/rest-client/drives.go
@@ -0,0 +1,40 @@
+package client
+
+
+type Drive struct {
+	Name          string    `json:"name"`
+	Details       string    `json:"details"`
+	Capacity      float64   `json:"capacity"`
+	Protocol      string    `json:"protocol"`
+	Status        string    `json:"status"`
+	Type          string    `json:"type"`
+}
+
+type DriveList struct {
+        ContinuationToken    string      `json:"continuation_token"`
+        TotalItemCount       int         `json:"total_item_count"`
+        MoreItemsRemaining   bool        `json:"more_items_remaining"`
+	Items                []Drive     `json:"items"`
+}
+
+func (fa *FAClient) GetDrives() *DriveList {
+	uri := "/drives"
+	result := new(DriveList)
+	res, err := fa.RestClient.R().
+		SetResult(&result).
+		Get(uri)
+	if err != nil {
+		fa.Error = err
+	}
+        if res.StatusCode() == 401 {
+                fa.RefreshSession()
+        }
+	res, err = fa.RestClient.R().
+		SetResult(&result).
+		Get(uri)
+        if err != nil {
+                fa.Error = err
+        }
+
+	return result
+}
