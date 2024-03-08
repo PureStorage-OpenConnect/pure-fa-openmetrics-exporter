328f323f4d77a76267782ebc8d51022cd437bc89
Simon Dodsley <simon@purestorage.com>
2024-03-08 09:25:32 -0500

    Update ports_test.go

--- internal/rest-client/ports_test.go
+++ internal/rest-client/ports_test.go
@@ -35,7 +35,7 @@ func TestPorts(t *testing.T) {
 	e := endp[len(endp)-1]
 	t.Run("ports_1", func(t *testing.T) {
 		defer server.Close()
-		c := NewRestClient(e, "fake-api-token", "latest", false)
+		c := NewRestClient(e, "fake-api-token", "latest", "test-user-agent-string", false)
 		pl := c.GetPorts()
 		if diff := cmp.Diff(pl.Items, ports.Items); diff != "" {
 			t.Errorf("Mismatch (-want +got):\n%s", diff)
