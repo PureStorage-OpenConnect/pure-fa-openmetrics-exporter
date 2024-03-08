abe5ffccfb49c0ef5abe14787005ee80396bd8fb
Simon Dodsley <simon@purestorage.com>
2024-03-08 09:25:12 -0500

    Update pods_test.go

--- internal/rest-client/pods_test.go
+++ internal/rest-client/pods_test.go
@@ -36,7 +36,7 @@ func TestPods(t *testing.T) {
         e := endp[len(endp)-1]
         t.Run("pods_1", func(t *testing.T) {
             defer server.Close()
-            c := NewRestClient(e, "fake-api-token", "latest", false)
+            c := NewRestClient(e, "fake-api-token", "latest", "test-user-agent-string", false)
 	    pl := c.GetPods()
 	    if diff := cmp.Diff(pl.Items, pods.Items); diff != "" {
                 t.Errorf("Mismatch (-want +got):\n%s", diff)
