493529bc090309cff6cf9c9c9444050a92e55c63
Simon Dodsley <simon@purestorage.com>
2024-03-08 09:21:15 -0500

    Update drives_test.go

--- internal/rest-client/drives_test.go
+++ internal/rest-client/drives_test.go
@@ -36,7 +36,7 @@ func TestDrive(t *testing.T) {
         e := endp[len(endp)-1]
         t.Run("drive_1", func(t *testing.T) {
             defer server.Close()
-            c := NewRestClient(e, "fake-api-token", "latest", false)
+            c := NewRestClient(e, "fake-api-token", "latest", "test-user-agent-string", false)
 	    dl := c.GetDrives()
 	    if diff := cmp.Diff(dl.Items, dr.Items); diff != "" {
                 t.Errorf("Mismatch (-want +got):\n%s", diff)
